1d78f2779c4ddeea8ec6b99d54ccb6f3e18ce0b2
Arseny Sokolov <me@arsen.pw>
2020-04-09 18:48:29 +0800

    Полностью изменено представление страницы с сеткой брони.

--- app/booking/views.py
+++ app/booking/views.py
@@ -1,7 +1,12 @@
-from datetime import datetime
+from datetime import date, timedelta
+from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.utils import timezone
-from ..rooms.models import Category
+from django.utils.decorators import method_decorator
+from .forms import BookingManageForm
+from .utils import get_booking_table, GanttData
+from ..rooms.models import Room
+from .models import Booking
 
 
 class IndexView(generic.TemplateView):
@@ -9,30 +14,49 @@ class IndexView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         kwargs = super(IndexView, self).get_context_data(**kwargs)
-        categories = Category.objects.all()
-        booking_table = []
-        for category in categories:
-            add_room = True
-            booking_table.append({
-                'name': category.name
-            })
-            for room in category.room_set.all():
-                values = []
-                if room.booking_set.filter(date_to__gte=timezone.now()).count():
-                    for value in room.booking_set.filter(date_to__gte=timezone.now()):
-                        values.append({
-                            'from': value.date_from.strftime('%Y/%m/%d'),
-                            'to': value.date_to.strftime('%Y/%m/%d'),
-                        })
-                if add_room:
-                    booking_table[-1]['desc'] = room.name
-                    booking_table[-1]['values'] = values
-                    add_room = False
-                else:
-                    booking_table.append({
-                        'desc': room.name,
-                        'values': values
-                    })
+        try:
+            period = abs(int(self.request.GET.get('period', 90)))
+        except ValueError:
+            period = 90
+        else:
+            if period < 1 or period > 90:
+                period = 90
+        now = timezone.now()
+        start_date = now - timedelta(days=2)
+        end_date = now + timedelta(days=period+2)
 
+        kwargs.update({
+            'now_date': now,
+            'start_date': start_date,
+            'end_date': end_date,
+            'rooms': Room.objects.filter(is_active=True).values('id', 'name', 'category__name'),
+            'booking': Booking.objects.filter(date_to__gte=start_date, date_from__lte=end_date),
+        })
+        return kwargs
+
+
+@method_decorator(login_required(login_url='/manage/login'), name='dispatch')
+class DashboardView(generic.TemplateView):
+    template_name = 'manage/index.html'
+    extra_context = {
+        'title': 'Сетка брони',
+        'title_icon': 'calendar alternate outline',
+        'subtitle': 'Вся статистика по бронированию за текущий месяц',
+    }
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(DashboardView, self).get_context_data(**kwargs)
+        booking_table = get_booking_table()
         kwargs.update({'booking_table': booking_table})
         return kwargs
+
+
+@method_decorator(login_required(login_url='/manage/login'), name='dispatch')
+class AddReservation(generic.CreateView):
+    template_name = 'manage/add_reservation.html'
+    form_class = BookingManageForm
+    extra_context = {
+        'title':  'Новое бронирование',
+        'title_icon': 'calendar plus outline',
+        'subtitle': 'Информация о заезде',
+    }
