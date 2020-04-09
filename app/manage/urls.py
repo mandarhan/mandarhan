8d7e0dbd0efd8a245c3784096bf09ebc02e77d64
Arseny Sokolov <me@arsen.pw>
2020-04-09 18:49:40 +0800

    Добавлены ендпоинты для кабинета менеджеров: добавление брони, главная кабинете и выход из кабинета.

--- app/manage/urls.py
+++ app/manage/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
+from ..booking import views as booking_views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='dashboard'),
+    path('', booking_views.DashboardView.as_view(), name='dashboard'),
+    path('add-reservation', booking_views.AddReservation.as_view(), name='add-reservation'),
     path('login', views.LoginView.as_view(), name='login'),
+    path('logout', views.LogoutView.as_view(), name='logout'),
 ]
